7cb1349658c3040e298ab54c449428637a538fbb
achillesprojects07-bit <achilles.projects07@gmail.com>
2026-09-04 15:40:30 +0800

    Cache admin dashboard and branding

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='smf-v5-shell-4';
-const SHELL=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest'];
+const CACHE='smf-v5-shell-5';
+const SHELL=['./','./index.html','./styles.css','./brand.css','./app.js','./config.js','./manifest.webmanifest','./admin.html','./admin.css','./admin.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
