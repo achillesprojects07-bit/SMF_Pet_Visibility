5126715273dcb050b4c5f097e5a5a6f86ec32cfe
achillesprojects07-bit <achilles.projects07@gmail.com>
2026-09-04 20:49:42 +0800

    Point field photo uploads to V6 direct Google Worker

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.SMF_CONFIG = {
-  API_BASE_URL: 'https://smf-pet-visibility-api.achilles-projects07.workers.dev'
+  API_BASE_URL: 'https://smf-pet-visibility-api.achilles-projects07.workers.dev',
+  PHOTO_API_BASE_URL: 'https://smf-pet-visibility-photo-v6.achilles-projects07.workers.dev'
 };
