c8945685c368eb9d59587ce5d3be3c920fddb674
achillesprojects07-bit <achilles.projects07@gmail.com>
2026-09-04 21:06:38 +0800

    Keep live field photos on stable V5 until V6 OAuth passes readiness

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.SMF_CONFIG = {
-  API_BASE_URL: 'https://smf-pet-visibility-api.achilles-projects07.workers.dev',
-  PHOTO_API_BASE_URL: 'https://smf-pet-visibility-photo-v6.achilles-projects07.workers.dev'
+  API_BASE_URL: 'https://smf-pet-visibility-api.achilles-projects07.workers.dev'
 };
